031ec220723446b93e91faecc52787aa4f63a0a3
之一 <nicolanazwj@gmail.com>
2022-08-03 00:32:25 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { ConfigProvider, DatePicker, message } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+import 'antd/dist/antd.css';
+import EditTable from "./EditTable";
 
-function App() {
+moment.locale('zh-cn');
+
+const App = () => {
+  const [date, setDate] = useState(null);
+  const handleChange = (value: any) => {
+    message.info(`您选择的日期是: ${value ? value.format('YYYY年MM月DD日') : '未选择'}`);
+    setDate(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <div style={{ width: "1000px", margin: "100px auto" }}>
+        <EditTable />
+      </div>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
